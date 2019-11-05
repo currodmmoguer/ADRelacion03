beda5ef672b1ee943c524beeef391f6d4e219d8b
Francisco <currodmmoguer@gmail.com>
2019-11-05 16:01:43 +0100

    -

--- src/ejercicio1/NotasAlumnosDAO.java
+++ src/ejercicio1/NotasAlumnosDAO.java
@@ -4,32 +4,72 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class NotasAlumnosDAO {
 
-	private static final String URL = "jdbc:sqlite:db/Ejercicio1/DBEjercicio1.db";
+	private static final String URL = "jdbc:sqlite:db/Ejercicio1/BDEjercicio1.db";
 	private static final String DRIVER = "org.sqlite.JDBC";
 	private Connection conexion;
-	
+
 	public NotasAlumnosDAO() throws SQLException, ClassNotFoundException {
 		Class.forName(DRIVER);
 		conexion = DriverManager.getConnection(URL);
-		
+
 	}
-	
-	public void restarNotaPorFaltas() {
+
+	public void restarNotaPorFaltas() throws SQLException {
+		String dni, asignatura;
+		int nota;
+		ResultSet resultAlumnos, resultNotas;
+		Statement sentenciaAlumnos, sentenciaNota, sentenciaActualizar;
+
+		sentenciaAlumnos = conexion.createStatement();
+		sentenciaNota = conexion.createStatement();
+		sentenciaActualizar = conexion.createStatement();
 		
+		resultAlumnos = sentenciaAlumnos.executeQuery("select * from alumnos where faltas > 20");
+
+		while (resultAlumnos.next()) {
+			dni = resultAlumnos.getString("dni");
+			
+
+			resultNotas = sentenciaNota.executeQuery("select * from notas where dni = '" 
+					+ dni + "'");
+
+			while (resultNotas.next()) {
+
+				nota = resultNotas.getInt("nota");
+				asignatura = resultNotas.getString("asig");
+
+				sentenciaActualizar.executeUpdate(
+						"update notas set nota = " + (nota -1) + " where dni = '" + dni + 
+						"' and nota > 0 and asig = '" + asignatura + "'");
+			}
+
+		}
+
+		sentenciaAlumnos.close();
 	}
-	
-	public String mostrarNotas() {
-		
-		
-		
-		
-		return null;
-		
+
+	public String mostrarNotas() throws SQLException {
+		StringBuilder datos = new StringBuilder();
+		ResultSet result;
+
+		Statement sentencia = conexion.createStatement();
+		result = sentencia.executeQuery("select * from notas;");
+
+		while (result.next()) {
+			datos.append("Alumno: " + result.getString("dni") + "\tAsignatura: " + result.getString("asig")
+					+ "\t\tNota: " + result.getInt("nota") + "\n");
+		}
+
+		sentencia.close();
+
+		return datos.toString();
+
 	}
-	
+
 	public void cerrarConexion() throws SQLException {
 		if (conexion != null)
 			conexion.close();
--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -8,6 +8,11 @@ public class Principal {
 		
 		try {
 			NotasAlumnosDAO dao = new NotasAlumnosDAO();
+			System.out.println(dao.mostrarNotas());
+			dao.restarNotaPorFaltas();
+			System.out.println();
+			System.out.println(dao.mostrarNotas());
+			
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
